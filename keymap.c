ea1a149b3cdd036333412bb438d7e2d2a06f3c65
Brendan McCourt <bcmccourt202@gmail.com>
2023-05-27 17:11:20 -0500

    Added Pause/Break to Layer 1+2;changed RALT to LALT

--- keymap.c
+++ keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R , KC_T , KC_Y , KC_U , KC_I   , KC_O   , KC_P   , KC_DEL, 
 		KC_ESC , KC_A   , KC_S   , KC_D   , KC_F , KC_G , KC_H , KC_J , KC_K   , KC_L   , KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V , KC_B , KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_ENT,
-		KC_LCTL, KC_RSFT, KC_LGUI, KC_RALT, MO(1),    KC_SPC   , MO(2), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
+		KC_LCTL, KC_RSFT, KC_LGUI, KC_LALT, MO(1),    KC_SPC   , MO(2), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
 
 /* Layer 1
  * ,-----------------------------------------------------------------------------------.
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   |  |   |  | Home |  End |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |RShift| Mod  | Alt  |Lower |    Space    |MO(3) | Mute | Prev | Next | Play |
+ * | Ctrl | Pause| Mod  | Alt  |Lower |    Space    |MO(3) | Mute | Prev | Next | Play |
  * `-----------------------------------------------------------------------------------'
  */
 	[1] = LAYOUT_preonic_1x2uC(
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
 			KC_CAPS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_INS,
 			KC_LSFT, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_PIPE, KC_PIPE, KC_HOME, KC_END , KC_ENT,
-			KC_LCTL, KC_RSFT, KC_LGUI, KC_RALT, KC_TRNS,     KC_SPC      , MO(3)  , KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY),
+			KC_LCTL, KC_PAUS, KC_LGUI, KC_LALT, KC_TRNS,     KC_SPC      , MO(3)  , KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY),
 
 /* Layer 2
  * ,-----------------------------------------------------------------------------------.
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   \  |   \  | PGUP |PGDOWN|Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |RShift| Mod  | Alt  |MO(3) |    Space    |Raise | Mute | Vol- | Vol+ | Play |
+ * | Ctrl | Pause| Mod  | Alt  |MO(3) |    Space    |Raise | Mute | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 	[2] = LAYOUT_preonic_1x2uC(
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_GRV , KC_1   , KC_2   , KC_3   , KC_4  , KC_5  , KC_6  , KC_7   , KC_8   , KC_9   , KC_0   , KC_DEL,
 			KC_PSCR, KC_F1  , KC_F2  , KC_F3  , KC_F4 , KC_F5 , KC_F6 , KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_PSLS,
 			KC_LSFT, KC_F7  , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12, KC_BSLS, KC_BSLS, KC_PGUP, KC_PGDN, KC_ENT,
-			KC_LCTL, KC_RSFT, KC_LGUI, KC_RALT, MO(3) ,    KC_SPC     , KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY),
+			KC_LCTL, KC_PAUS, KC_LGUI, KC_LALT, MO(3) ,    KC_SPC     , KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY),
 
 /* Layer 3 (Debug)
  * ,-----------------------------------------------------------------------------------.
